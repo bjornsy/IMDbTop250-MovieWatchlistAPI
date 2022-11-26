aa3c7468ade59b83560b00a0c70e86da382fc2c0
Bjorn <bjorn.sydenham@yahoo.co.uk>
2022-11-26 20:08:10 +0000

    Readding AddWatchlistSchema

---
+++ src/MovieWatchlist.Infrastructure/Migrations/20221126200601_AddWatchlistSchema.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MovieWatchlist.Infrastructure.Migrations
+{
+    public partial class AddWatchlistSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Watchlists");
+
+            migrationBuilder.CreateTable(
+                name: "Watchlists",
+                schema: "Watchlists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Watchlists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchlistsMovies",
+                schema: "Watchlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WatchlistId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MovieId = table.Column<string>(type: "text", nullable: false),
+                    Watched = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchlistsMovies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Watchlists",
+                schema: "Watchlists");
+
+            migrationBuilder.DropTable(
+                name: "WatchlistsMovies",
+                schema: "Watchlists");
+        }
+    }
+}
